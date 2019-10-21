b45d4ae77364e828d3bd5b9fd86eee61b6a922be
Joe Heffernan <jheffheffer@gmail.com>
2019-10-21 08:21:30 -0500

    Cleaned up code, removed uneeded variables.

--- convert_spss_to_redcap.R
+++ convert_spss_to_redcap.R
@@ -4,6 +4,62 @@ library(tidyverse)
 # manually assign the numbers to these variables
 # if I cannot determine the number, go to Scott
 
+# non-repeated instruments do not have redcap_repeat_instrument or redcap_repeat_instance
+
+prepare_instrument_data <- function(instrument, data) {
+  cat("Instrument: ", instrument, "\n")
+
+  if (is.na(instrument)) {
+
+    out <- data
+
+  } else if (instrument == "demographics") {
+
+    out <- data %>%
+      mutate(redcap_repeat_instrument = instrument) %>%
+      select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything())
+
+  } else if (instrument == "neurology") {
+
+    out <- data %>%
+      mutate(redcap_repeat_instrument = instrument) %>%
+      select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything())
+
+  } else if (instrument == "baseline_testing") {
+    
+    out <- data %>%
+      mutate(redcap_repeat_instrument = "") %>%
+      select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything())
+
+  } else if (instrument == "neuropsych_tests") {
+
+    out <- data %>%
+      mutate(redcap_repeat_instrument = instrument) %>%
+      select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything()) %>%
+      left_join(has_subtests, by = c("record_id", "redcap_repeat_instance"))
+
+  } else if (instrument == "neuroreader") {
+
+    out <- data %>%
+      mutate(redcap_repeat_instrument = instrument) %>%
+      select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything())
+
+  } else if (instrument == "other") {
+
+    out <- data %>%
+      mutate(redcap_repeat_instrument = "") %>%
+      select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything())
+
+  } else {
+
+    stop("Unknown instrument: ", instrument)
+
+  }
+
+  return(out)
+}
+  
+
 data <- read_tsv("/home/jheffernan/test.tsv")
 
 record_ids <- data %>%
@@ -78,6 +134,7 @@ has_subtests <- data_long %>%
   ungroup() %>%
   spread(subtest, has)
 
+# compare_counts
 counts_all <- data_long %>%
   select(form_name, redcap_repeat_instance, redcap, key) %>%
   distinct() %>%
@@ -91,19 +148,37 @@ counts_values <- data_long %>%
   count(form_name, redcap_repeat_instance, name = "has_value") %>%
   filter(!is.na(form_name))
 
+counts_participants <- data_long %>% 
+  filter(!is.na(value)) %>% 
+  select(record_id, form_name, redcap_repeat_instance) %>% 
+  distinct() %>% 
+  count(form_name, redcap_repeat_instance, name = "participants_with_values")
+
 # this is important; show this variable to people
 # missing values in has_values mean zero of those variables were filed for that instance
 # displays the number of variables for that instance and form
 # displays the variables with values for that instance and form
 compare_counts <- counts_all %>%
-  left_join(counts_values, by = c("form_name", "redcap_repeat_instance"))
+  left_join(counts_values, by = c("form_name", "redcap_repeat_instance")) %>%
+  left_join(counts_participants, by = c("form_name", "redcap_repeat_instance")) %>%
+  mutate(has_value = ifelse(is.na(has_value), 0, has_value),
+         participants_with_values = ifelse(is.na(participants_with_values), 0, participants_with_values))
+
+write_csv(compare_counts, "info_compare_counts.csv")
 
 # write each form_name out individually
 nested_form_names <- data_long %>%
   select(-key) %>%
   nest_legacy(-form_name) %>%
   mutate(data = map(data, spread, redcap, value),
-         data = map(data, filter_at, vars(-record_id, -redcap_repeat_instance), any_vars(!is.na(.))))
+         data = map(data, filter_at, vars(-record_id, -redcap_repeat_instance), any_vars(!is.na(.))),
+         prepared_data = map2(form_name, data, prepare_instrument_data))
+
+# write out instruments
+nested_form_names %>%
+  mutate(file_name = paste0("tp-all_", form_name, ".csv"),
+         prepared_data = walk2(prepared_data, file_name, write_csv))
+  
 
 # get variables with missing instances
 redcap_with_missing_instances <- data_long %>%
@@ -111,127 +186,22 @@ redcap_with_missing_instances <- data_long %>%
   select(redcap, key, form_name, redcap_repeat_instance) %>%
   distinct()    
 
-# DEMOGRAPHICS (keep all instances for now)
-# some variables have multiple instances and currently it is not a repeated
-# instrument on redcap, but let us include all instances anyways
+# write out variables missing from redcap
+missing_from_redcap <- data_long %>%
+  filter(is.na(form_name)) %>%
+  select(key, redcap, redcap_repeat_instance, form_name) %>%
+  distinct()
 
-check_equal_repeated_demographics <- data_long %>%
-  filter(form_name == "demographics", !is.na(value)) %>%
-  add_count(record_id, redcap) %>%
-  filter(n > 1) %>%
-  group_by(record_id, redcap) %>%
-  summarize(all_equal = all(value == first(value)))
+write_csv(missing_from_redcap, "info_missing_from_redcap.csv")
 
-unequal_repeated_demographics <- data_long %>%
-  semi_join(check_equal_repeated_demographics %>% filter(!all_equal), by = c("record_id", "redcap"))
+# write out extra varialbes in redcap
+extra_in_redcap <- data_dictionary %>%
+  anti_join(data_long, by = "redcap") %>%
+  select(redcap, form_name)
 
-repeated_demographics <- data_long %>%
-  filter(form_name == "demographics") %>%
-  count(redcap, redcap_repeat_instance)
-
-demographics <- nested_form_names$data[[2]]
-
-all_demographics <- demographics %>%
-  mutate(redcap_repeat_instrument = "demographics") %>%
-  select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything())
-
-all_demographics %>%
-  write_csv("tp-all_demographics.csv", na = "")
-
-# NEUROLOGY (keep all instances, there are some variables with missing instances)
-# some variables have multiple instances and currently it is not a repeated
-# instrument on redcap
-
-# write out all form redcap variables, their instances, and whether the variable has values
-data_long %>%
-  filter(!form_name %in% c("neuropsych_tests", "neuroreader")) %>%
-  group_by(form_name, redcap, redcap_repeat_instance) %>%
-  summarize(has_value = sum(!is.na(value)), total = n()) %>%
-  write_csv("instances_in_spss.csv")
-
-data_long %>%
-  filter(!form_name %in% c("neuropsych_tests", "neuroreader")) %>%
-  group_by(form_name, redcap, redcap_repeat_instance) %>%
-  summarize(has_value = sum(!is.na(value)), total = n()) %>%
-  filter(has_value != 0) %>%
-  write_csv("instances_with_values.csv")
-
-data_long %>%
-  filter(form_name == "neuropsych_tests") %>%
-  group_by(form_name, redcap, redcap_repeat_instance) %>%
-  summarize(has_value = sum(!is.na(value)), total = n()) %>%
-  filter(has_value != 0, redcap_repeat_instance %in% c(0, 1)) %>%
-  write_csv("neuropsych_with_baseline.csv")
-
-neurology <- nested_form_names$data[[3]]
-
-all_neurology <- neurology %>%
-  mutate(redcap_repeat_instrument = "neurology") %>%
-  select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything())
-
-all_neurology %>%
-  filter(!is.na(redcap_repeat_instance)) %>%
-  write_csv("tp-all_neurology.csv", na = "")
-
-# BASELINE TESTING 
-#   looks fine from instances_with_values.csv
-#   just filter instance == NA
-baseline_testing <- nested_form_names$data[[5]]
-
-all_baseline_testing <- baseline_testing %>%
-  mutate(redcap_repeat_instrument = "baseline_testing") %>%
-  select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything())
-
-all_baseline_testing %>%
-  select(-redcap_repeat_instrument) %>%
-  write_csv("tp-all_baseline_testing.csv", na = "")
-
-# NEUROPSYCH TESTS
-
-# there should always be a timepoint for neuropsych tests
-# it was decided that _BL = 1, _1 = 2, _2 = 3, and so on
-# this will be used for all instruments
-
-# write out test neurpsych_tests upload data
-neuropsych_tests <- nested_form_names$data[[4]]
-
-all_neuropsych_tests <- neuropsych_tests %>%
-  filter(!is.na(redcap_repeat_instance)) %>% # there shoud always be a timepoint for neuropsych_tests
-  mutate(redcap_repeat_instrument = "neuropsych_tests") %>%
-  select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything()) %>%
-  left_join(has_subtests, by = c("record_id", "redcap_repeat_instance"))
-
-all_neuropsych_tests %>%
-  write_csv("tp-all_neurospych_tests.csv", na = "")
-
-# NEUROREADER
-neuroreader <- nested_form_names$data[[6]]
-
-all_neuroreader <- neuroreader %>%
-  filter(!is.na(redcap_repeat_instance), redcap_repeat_instance != 0) %>%
-  mutate(redcap_repeat_instrument = "neuroreader") %>%
-  select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything()) %>%
-  filter_at(vars(-record_id, -redcap_repeat_instrument, -redcap_repeat_instance),
-            any_vars(!is.na(.)))
-
-all_neuroreader %>%
-  write_csv("tp-all_neuroreader.csv", na = "")
-
-
-# OTHER
-#   looks file from instances_with_values.csv
-#   just filter instance == NA
-other <- nested_form_names$data[[7]]
-
-all_other <- other %>%
-  mutate(redcap_repeat_instrument = "other") %>%
-  select(record_id, redcap_repeat_instrument, redcap_repeat_instance, everything())
-
-all_other %>%
-  select(-redcap_repeat_instrument) %>%
-  write_csv("tp-all_other.csv", na = "")
-
+write_csv(extra_in_redcap, "info_extra_in_redcap.csv")
   
+
 
 # demographics     - has multiple instances, not a repeated instrument
 # neurology        - has multiple instances, not a repeated instrument
@@ -240,14 +210,3 @@ all_other %>%
 # neuroreader      - looks good
 # other            - looks good
   
-
-
-
-
-  
-  
-  
-
-
-
-
